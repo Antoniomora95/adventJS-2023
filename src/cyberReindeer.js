4057eb5b4f5c274ead1091c322557f42aaf9036c
Antonio Mora <antonio.mora@oracle.com>
2023-12-20 18:05:55 -0600

    clean-algorithm

--- src/cyberReindeer.js
+++ src/cyberReindeer.js
@@ -7,26 +7,20 @@ export default function cyberReindeer(road, time) {
 
     let cleanRoadSate = road.replace(sled, roadUnit);
     let currentIndex = road.indexOf(sled);
-    let countToOpen = 5;
 
     let modifiedRoad = road;
 
     for (let index = 0; index < time; index += 1) {
-        // check if i can modify the string, meaning if can or cannot move.
-
-        if (countToOpen === 0) {
+        if (index === 5) {
             cleanRoadSate = cleanRoadSate.replace(/\|/g, openBarrier);
         }
         const canGoAhead = cleanRoadSate.charAt(currentIndex + 1) !== closedBarrier;
-
         if (canGoAhead && index > 0) {
             modifiedRoad = cleanRoadSate.substring(0, currentIndex + 1)
             + sled + cleanRoadSate.substring(currentIndex + 2);
 
             currentIndex += 1;
         }
-
-        countToOpen -= 1;
 
         finalResult.push(modifiedRoad);
     }
